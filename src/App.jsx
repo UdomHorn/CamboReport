92a4f1d0f46e308e7582a1daffc7e97fdc304eaf
udom04 <udomhorn04@gmail.com>
2025-04-19 21:58:49 +0700

    add nav to small device

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Nav from './section/Nav'
 import Home from './section/Home'
 import Footer from './section/Footer'
 // import Video from './section/component/video'
-import {Video, Tablet, Knowledge, Movie, Science, Software, Telephone, Vehicle, Electronic} from './section/component/index'
+import { Video, Tablet, Knowledge, Movie, Science, Software, Telephone, Vehicle, Electronic } from './section/component/index'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
@@ -11,18 +11,18 @@ function App() {
 
       <Nav />
 
-          <Routes >
-            <Route path="/" element={<Home />} />
-            <Route path="/videos" element={<Video />} />
-            <Route path="/tablets" element={<Tablet />} />
-            <Route path="/movies" element={<Movie />} />
-            <Route path="/telephones" element={<Telephone />} />
-            <Route path="/general" element={<Knowledge />} />
-            <Route path="/vehicles" element={<Vehicle />} />
-            <Route path="/sciences" element={<Science />} />
-            <Route path="/softwaregames" element={<Software />} />
-            <Route path="/electronic" element={<Electronic />} />
-          </Routes>
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Video />} />
+        <Route path="/tablets" element={<Tablet />} />
+        <Route path="/movies" element={<Movie />} />
+        <Route path="/telephones" element={<Telephone />} />
+        <Route path="/general" element={<Knowledge />} />
+        <Route path="/vehicles" element={<Vehicle />} />
+        <Route path="/sciences" element={<Science />} />
+        <Route path="/softwaregames" element={<Software />} />
+        <Route path="/electronic" element={<Electronic />} />
+      </Routes>
 
       <Footer />
 
--- src/section/Nav.jsx
+++ src/section/Nav.jsx
@@ -8,27 +8,42 @@ const Nav = () => {
 
         <div className=' flex justify-between items-center xl:w-[75%]   max-2xl:w-[96%] m-1  '>
           <div className='flex items-center gap-2 cursor-pointer'>
-            <NavLink to="/"><img src={logo} alt="" className='w-[44px] h-[44px]'/></NavLink>
+            <NavLink to="/"><img src={logo} alt="" className='w-[44px] h-[44px]' /></NavLink>
             <div className='lg:hidden text-xl'>ខេមបូរីផត</div>
           </div>
-          <ul className='m-4 max-lg:hidden'>
-            <li className='flex gap-2 items-center'>
-              <NavLink to="/videos" className='hover:text-orange-500  duration-200' >វីដេអូ</NavLink> /
-              <NavLink to="/tablets" className='hover:text-orange-500  duration-200' > ថេប្លេត-កុំព្យូទ័រ</NavLink> /
-              <NavLink to="/movies" className='hover:text-orange-500  duration-200' > ភាពយន្ត</NavLink> /
-              <NavLink to="/telephones" className='hover:text-orange-500  duration-200' > ទូរសព្ទ</NavLink> /
-              <NavLink to="/general" className='hover:text-orange-500  duration-200' > ចំណេះដឹងទូទៅ</NavLink> /
-              <NavLink to="/vehicles" className='hover:text-orange-500  duration-200' > យានយន្ត</NavLink> /
-              <NavLink to="/sciences" className='hover:text-orange-500  duration-200' > វិទ្យាសាស្រ្ត</NavLink> /
-              <NavLink to="/softwaregames" className='hover:text-orange-500  duration-200' > កម្មវិធី-ហ្គេម</NavLink> /
-              <NavLink to="/electronic" className='hover:text-orange-500  duration-200' > ឧប្បករណ៍អេឡិចត្រូនិក</NavLink> /
-            </li>
+          <ul className='m-4 max-lg:hidden flex gap-2 items-center'>
+            <li className=''><NavLink to="/videos" className='hover:text-orange-500  duration-200' >វីដេអូ</NavLink> </li> /
+            <li><NavLink to="/tablets" className='hover:text-orange-500  duration-200' > ថេប្លេត-កុំព្យូទ័រ</NavLink></li> /
+            <li><NavLink to="/movies" className='hover:text-orange-500  duration-200' > ភាពយន្ត</NavLink></li> /
+            <li><NavLink to="/telephones" className='hover:text-orange-500  duration-200' > ទូរសព្ទ</NavLink></li> /
+            <li><NavLink to="/general" className='hover:text-orange-500  duration-200' > ចំណេះដឹងទូទៅ</NavLink></li> /
+            <li><NavLink to="/vehicles" className='hover:text-orange-500  duration-200' > យានយន្ត</NavLink></li> /
+            <li><NavLink to="/sciences" className='hover:text-orange-500  duration-200' > វិទ្យាសាស្រ្ត</NavLink></li> /
+            <li><NavLink to="/softwaregames" className='hover:text-orange-500  duration-200' > កម្មវិធី-ហ្គេម</NavLink></li> /
+            <li><NavLink to="/electronic" className='hover:text-orange-500  duration-200' > ឧប្បករណ៍អេឡិចត្រូនិក</NavLink></li>
+
           </ul>
           <div className='flex items-center gap-2'>
             <div className=' w-[44px] h-[44px] rounded-full border-2 border-solid border-gray-200 max-sm:hidden'></div>
             <div className='lg:hidden h-[40px] w-[52px] rounded-md  border-2 border-solid border-gray-200'> </div>
+
+
           </div>
         </div>
+      </div>
+      <div className='bg-white font-koulen text-lg lg:hidden p-8'>
+        <ul className=' right-10 '>
+          <li className='mb-2'><NavLink to="/" className='hover:text-orange-500  duration-200' >ទំព័រដើម</NavLink></li>
+          <li className='mb-2'><NavLink to="/videos" className='hover:text-orange-500  duration-200' >វីដេអូ</NavLink> </li>
+          <li className='mb-2'><NavLink to="/tablets" className='hover:text-orange-500  duration-200' > ថេប្លេត-កុំព្យូទ័រ</NavLink></li>
+          <li className='mb-2'><NavLink to="/movies" className='hover:text-orange-500  duration-200' > ភាពយន្ត</NavLink></li>
+          <li className='mb-2'><NavLink to="/telephones" className='hover:text-orange-500  duration-200' > ទូរសព្ទ</NavLink></li>
+          <li className='mb-2'><NavLink to="/general" className='hover:text-orange-500  duration-200' > ចំណេះដឹងទូទៅ</NavLink></li>
+          <li className='mb-2'><NavLink to="/vehicles" className='hover:text-orange-500  duration-200' > យានយន្ត</NavLink></li>
+          <li className='mb-2'><NavLink to="/sciences" className='hover:text-orange-500  duration-200' > វិទ្យាសាស្រ្ត</NavLink></li>
+          <li className='mb-2'><NavLink to="/softwaregames" className='hover:text-orange-500  duration-200' > កម្មវិធី-ហ្គេម</NavLink></li>
+          <li className='mb-2'><NavLink to="/electronic" className='hover:text-orange-500  duration-200' > ឧប្បករណ៍អេឡិចត្រូនិក</NavLink></li>
+        </ul>
       </div>
     </nav>
   )
